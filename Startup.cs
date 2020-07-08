0d438da3273d3adaffa199bf5cd5e3ce9d30a215
Jade Rickerts <jade.rickerts@live.com>
2020-07-08 13:44:46 +0200

    typo in dbcontext

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace Commander
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
-            (Configuration.GetConnectionString("CommanderConnection")));
+                (Configuration.GetConnectionString("CommanderConnection")));
             services.AddControllers();
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
         }

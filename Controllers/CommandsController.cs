d174e0a16e08f8288c705f8758f75cc9cf2cc355
Lakendary <jade.rickerts@gmail.com>
2020-07-10 13:33:22 +0200

    add delete command action to controller

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -98,9 +98,25 @@ namespace Commander.Controllers
             _repository.UpdateCommand(commandModelFromRepo);
 
             _repository.SaveChanges();
-            
+
             return NoContent();
 
+        }
+
+        //DELETE api/commands/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCommand(int id)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if(commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _repository.DeleteCommand(commandModelFromRepo);
+
+            _repository.SaveChanges();
+
+            return NoContent();
         }
     }
 }
